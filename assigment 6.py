fdcd7ab83ba53bf79ac9bd87eb66d487d88f9c48
priyapranjali769-web <priyapranjali769@gmail.com>
2026-02-07 14:20:05 +0530

    Add files via upload

---
+++ assigment 6.py
@@ -0,0 +1,26 @@
+class Library:
+    def __init__(self):
+        self.books =[]
+        def add_book(self,book):
+            self.books.append(book)
+            def display_books(self):
+                if self.books:
+                    print("Books in the library:")
+                    for book in self.books:
+                        print(book)
+                    else:
+                        print("No books in the library.")
+                        #create library instance 
+                        Library = Library()
+                        #add books to library 
+                        Library.add_book("The Alchemist")
+                        Library.add_book("to kill a mockingbird")
+                        Library.add_book("1984")
+                        #display books in library 
+                        Library.display_books()
+
+
+
+
+
+l
